b01120b38d469a3686359cad84d6d7107a4381c8
fpizelli <168946758+fpizelli@users.noreply.github.com>
2025-01-15 22:28:56 -0300

    Initial commit

---
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -0,0 +1,325 @@
+<script setup lang="ts">
+import { Avatar, AvatarFallback, AvatarImage } from '@/Components/ui/avatar';
+
+import { Collapsible } from '@/Components/ui/collapsible';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuTrigger,
+} from '@/Components/ui/dropdown-menu';
+import { Separator } from '@/Components/ui/separator';
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarInset,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarProvider,
+    SidebarRail,
+    SidebarTrigger,
+} from '@/Components/ui/sidebar';
+import {
+    Bike,
+    ChartLine,
+    ChevronsUpDown,
+    CircleHelp,
+    CircleUserRound,
+    House,
+    LogOut,
+    Package,
+    PackagePlus,
+    ShoppingBasket,
+    ShoppingCart,
+} from 'lucide-vue-next';
+import { ref } from 'vue';
+
+// This is sample data.
+const data = {
+    user: {
+        name: 'Felipe',
+        email: 'felipe@duasrodas.com',
+        avatar: '/avatars/shadcn.jpg',
+    },
+    teams: [
+        {
+            name: 'Duas Rodas',
+            logo: Bike,
+            plan: 'Unidade 1',
+        },
+        {
+            name: 'Duas Rodas',
+            logo: Bike,
+            plan: 'Unidade 2',
+        },
+    ],
+    navMain: [
+        {
+            title: 'Visão geral',
+            url: '#',
+            icon: House,
+        },
+        {
+            title: 'Vendas',
+            url: '#',
+            icon: ShoppingCart,
+        },
+        {
+            title: 'Estoque',
+            url: '#',
+            icon: Package,
+        },
+        {
+            title: 'Análises',
+            url: '#',
+            icon: ChartLine,
+        },
+    ],
+    projects: [
+        {
+            name: 'Adicionar produto',
+            url: '#',
+            icon: PackagePlus,
+        },
+        {
+            name: 'Registrar venda',
+            url: '#',
+            icon: ShoppingBasket,
+        },
+    ],
+    misc: [
+        {
+            name: 'Help & Support',
+            url: '#',
+            icon: PackagePlus,
+        },
+        {
+            name: 'Settings',
+            url: '#',
+            icon: PackagePlus,
+        },
+    ],
+};
+
+const activeTeam = ref(data.teams[0]);
+
+function setActiveTeam(team: (typeof data.teams)[number]) {
+    activeTeam.value = team;
+}
+</script>
+
+<template>
+    <SidebarProvider>
+        <Sidebar collapsible="icon">
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger as-child>
+                                <SidebarMenuButton
+                                    size="lg"
+                                    class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                                >
+                                    <div
+                                        class="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground"
+                                    >
+                                        <component
+                                            :is="activeTeam.logo"
+                                            class="size-4"
+                                        />
+                                    </div>
+                                    <div
+                                        class="grid flex-1 text-left text-sm leading-tight"
+                                    >
+                                        <span class="truncate font-semibold">{{
+                                            activeTeam.name
+                                        }}</span>
+                                        <span class="truncate text-xs">{{
+                                            activeTeam.plan
+                                        }}</span>
+                                    </div>
+                                    <ChevronsUpDown class="ml-auto" />
+                                </SidebarMenuButton>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent
+                                class="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                                align="start"
+                                side="bottom"
+                                :side-offset="4"
+                            >
+                                <DropdownMenuLabel
+                                    class="text-xs text-muted-foreground"
+                                >
+                                    Lojas
+                                </DropdownMenuLabel>
+                                <DropdownMenuItem
+                                    v-for="(team, index) in data.teams"
+                                    :key="team.name"
+                                    class="gap-2 p-2"
+                                    @click="setActiveTeam(team)"
+                                >
+                                    <div
+                                        class="flex size-6 items-center justify-center rounded-sm border"
+                                    >
+                                        <component
+                                            :is="team.logo"
+                                            class="size-4 shrink-0"
+                                        />
+                                    </div>
+                                    {{ team.plan }}
+                                    <DropdownMenuShortcut
+                                        >⌘{{ index + 1 }}</DropdownMenuShortcut
+                                    >
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Plataforma</SidebarGroupLabel>
+                    <SidebarMenu>
+                        <Collapsible
+                            v-for="item in data.navMain"
+                            :key="item.title"
+                            as-child
+                            class="group/collapsible"
+                        >
+                            <SidebarMenuItem>
+                                <SidebarMenuButton :tooltip="item.title">
+                                    <component :is="item.icon" />
+                                    <span>{{ item.title }}</span>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        </Collapsible>
+                    </SidebarMenu>
+                </SidebarGroup>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Ações rápidas</SidebarGroupLabel>
+                    <SidebarMenu>
+                        <Collapsible
+                            v-for="item in data.projects"
+                            :key="item.name"
+                            as-child
+                            class="group/collapsible"
+                        >
+                            <SidebarMenuItem>
+                                <SidebarMenuButton :tooltip="item.name">
+                                    <component :is="item.icon" />
+                                    <span>{{ item.name }}</span>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        </Collapsible>
+                    </SidebarMenu>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger as-child>
+                                <SidebarMenuButton
+                                    size="lg"
+                                    class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                                >
+                                    <Avatar class="h-8 w-8 rounded-lg">
+                                        <AvatarImage
+                                            :src="data.user.avatar"
+                                            :alt="data.user.name"
+                                        />
+                                        <AvatarFallback class="rounded-lg">
+                                            F
+                                        </AvatarFallback>
+                                    </Avatar>
+                                    <div
+                                        class="grid flex-1 text-left text-sm leading-tight"
+                                    >
+                                        <span class="truncate font-semibold">{{
+                                            data.user.name
+                                        }}</span>
+                                        <span class="truncate text-xs">{{
+                                            data.user.email
+                                        }}</span>
+                                    </div>
+                                    <ChevronsUpDown class="ml-auto size-4" />
+                                </SidebarMenuButton>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent
+                                class="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                                side="bottom"
+                                align="end"
+                                :side-offset="4"
+                            >
+                                <DropdownMenuLabel class="p-0 font-normal">
+                                    <div
+                                        class="flex items-center gap-2 px-1 py-1.5 text-left text-sm"
+                                    >
+                                        <Avatar class="h-8 w-8 rounded-lg">
+                                            <AvatarImage
+                                                :src="data.user.avatar"
+                                                :alt="data.user.name"
+                                            />
+                                            <AvatarFallback class="rounded-lg">
+                                                CN
+                                            </AvatarFallback>
+                                        </Avatar>
+                                        <div
+                                            class="grid flex-1 text-left text-sm leading-tight"
+                                        >
+                                            <span
+                                                class="truncate font-semibold"
+                                                >{{ data.user.name }}</span
+                                            >
+                                            <span class="truncate text-xs">{{
+                                                data.user.email
+                                            }}</span>
+                                        </div>
+                                    </div>
+                                </DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuGroup>
+                                    <DropdownMenuItem>
+                                        <CircleHelp />
+                                        Suporte
+                                    </DropdownMenuItem>
+                                </DropdownMenuGroup>
+                                <DropdownMenuGroup>
+                                    <DropdownMenuItem>
+                                        <CircleUserRound />
+                                        Usuário
+                                    </DropdownMenuItem>
+                                </DropdownMenuGroup>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>
+                                    <LogOut />
+                                    Sair
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
+            <SidebarRail />
+        </Sidebar>
+        <SidebarInset>
+            <header
+                class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
+            >
+                <div class="flex items-center gap-2 px-4">
+                    <SidebarTrigger class="-ml-1" />
+                    <Separator orientation="vertical" class="mr-2 h-4" />
+                </div>
+            </header>
+        </SidebarInset>
+    </SidebarProvider>
+</template>
